c07232948e76bbc97c65db8340343f107c0b6c28
OoTibaltoO <jonathanboerio21@hotmail.fr>
2025-11-24 12:15:16 +0100

    Feat: Correction html

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
     base: "/React-Orecipes/",
     plugins: [react()],
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                app: resolve(__dirname, 'app.html')
-            }
-        }
-    }
 })
